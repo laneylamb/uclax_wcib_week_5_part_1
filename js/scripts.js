4f25be002ddacd8c52b99b24dbf8fafb1e097f6f
Delaney McKnight <delaneymcknight@Delaneys-MacBook-Pro-2.local>
2020-05-08 21:15:50 -0700

    Validation, attempt 2.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
-
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 
@@ -11,6 +10,7 @@ let guessCount = 1;
 let resetButton;
 
 function checkGuess() {
+
   let userGuess = Number(guessField.value);
   if (guessCount === 1) {
     guesses.textContent = 'Previous guesses: ';
